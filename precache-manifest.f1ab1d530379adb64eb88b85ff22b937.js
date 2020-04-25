d06e474026abebb8593bbf8f934e0be891589635
Sourav Sudhi <sourav.sudhi@gmail.com>
2020-04-25 16:27:59 +0530

    Updates

---
+++ precache-manifest.f1ab1d530379adb64eb88b85ff22b937.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b0ab427e205b1502886fca45049771",
+    "url": "/simpletodo/index.html"
+  },
+  {
+    "revision": "c733f64830ed7c88059a",
+    "url": "/simpletodo/static/css/main.823f08cf.chunk.css"
+  },
+  {
+    "revision": "4200bb5a4d4640827355",
+    "url": "/simpletodo/static/js/2.b66026db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simpletodo/static/js/2.b66026db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c733f64830ed7c88059a",
+    "url": "/simpletodo/static/js/main.5c62d60c.chunk.js"
+  },
+  {
+    "revision": "defb906ef8fed34d0d27",
+    "url": "/simpletodo/static/js/runtime-main.3128d6e8.js"
+  }
+]);
